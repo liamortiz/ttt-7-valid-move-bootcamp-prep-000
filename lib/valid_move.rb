0e03c86b370bf8c79ed84f10557ea9c2ff74cb28
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-09 20:39:31 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  position_taken(board, index)
+  position_taken?(board, index)
 end
 
 def position_taken?(board, index)
-  board[index] ? board.strip == "" : false
+  board[index] ? board.strip != "" : false
 end
